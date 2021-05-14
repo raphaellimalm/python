5e7c5f8818ec4c504b6dc02d94b29bcaef466219
Raphael Lima da Silva <72612872+raphaellimalm@users.noreply.github.com>
2021-05-14 17:23:24 -0300

    Add files via upload

---
+++ aula07.py
@@ -0,0 +1,16 @@
+class Calculadora:
+def_init_(self, num1, num2):
+        pass
+    def soma (self, valor_a, valor_b):
+        return self.valor_a + self.valor_b
+    def subtracao(salf, valor_a, valor_b):
+        return self.valor_a - self.valor_b
+    def multiplicacao(self, valor_a, valor_b):
+        return self.valor_a * self.valor_b
+    def divisao(self, valor_a, valor_b):
+        return self.valor_a / self.valor_b
+    Calculadora = Calculadora(10, 2)
+        print(calculadora.soma())
+        print(calculadora.sbtracao())
+        print(calculadora.divisao())
+        print(calculadora.multiplicacao())
